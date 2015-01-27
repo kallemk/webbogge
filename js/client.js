3cdff0e5e4e168cca23a34b9cc1e6709699bcc14
Kalle Olsson <kalleolsson92@gmail.com>
2015-01-27 15:04:32 +0100

    Form validation

--- js/client.js
+++ js/client.js
@@ -8,3 +8,11 @@ window.onload = function(){
     displayView("welcomeview");
  
 }
+
+function signupValidation(){
+	var x = document.getElementById("signup-pwd").value;
+	var y = document.getElementById("signup-pwd2").value;
+	if(x!=y){
+		window.alert("Both password fields must be the same");
+	}
+}
