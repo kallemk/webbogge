38b43dbad7db26ac5194899cc191fcee3543b065
Oskar Norberg <oskno129@student.liu.se>
2015-01-28 02:24:27 +0100

    complete form validation and object created

--- js/client.js
+++ js/client.js
@@ -10,9 +10,22 @@ window.onload = function(){
 }
 
 function signupValidation(){
-	var x = document.getElementById("signup-pwd").value;
-	var y = document.getElementById("signup-pwd2").value;
-	if(x!=y){
+	var x = 5; //Set the minimum character length of the password
+	var pwd1 = document.getElementById("signup-pwd").value;
+	var pwd2 = document.getElementById("signup-pwd2").value;
+	if(pwd1!=pwd2){
 		window.alert("Both password fields must be the same");
+	}else if ((pwd1.length || pwd2.length) < x){
+		window.alert("The password must contain at least " + x + " characters.");
+	}else{ //Should this really be here, maybe it should be moved to an own function. But I don't know how to call that yet. Kalle mar skit!
+		var dataObject = {
+			email : document.getElementById("signup-email").value,
+			password : document.getElementById("signup-pwd").value,
+			firstname : document.getElementById("firstname").value,
+			familyname : document.getElementById("lastname").value,
+			gender : document.getElementById("gender").value,
+			city : document.getElementById("city").value,
+			country : document.getElementById("country").value
+		};
 	}
 }
