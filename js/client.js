952a4686fa7dc6fd8db2ec8898e9abfbafa467f4
Oskar Norberg <oskno129@student.liu.se>
2015-02-04 12:31:03 +0100

    Browse tab search bar

--- js/client.js
+++ js/client.js
@@ -104,3 +104,16 @@ function changePassword(){
 	var pwdForm = document.getElementById('change-password');
 	pwdForm.reset();
 }
+
+function viewUser(){
+	var token = getToken();
+	var userEmail = document.getElementById('user-email').value;
+	alert(token);
+	var userData = serverstub.getUserDataByEmail(token, userEmail);
+	var userMessages = serverstub.getUserMessagesByEmail(token, userEmail);
+	if((userEmail.success && userData.success) === true){
+		alert(userData.message);
+	}else{
+		alert(userData.message);
+	}	
+}
