90163a6f0a791d2cab6efc7664fb4bcbb8146bec
Kalle Olsson <kalleolsson92@gmail.com>
2015-03-02 00:31:04 +0100

    Added customized alerts

--- js/client.js
+++ js/client.js
@@ -16,14 +16,14 @@ window.onload = function(){
     }
 }
 
-function signupValidation(){
+function signupValidation(view){
 	var x = 5; //Set the minimum character length of the password
 	var pwd1 = document.getElementById("signup-pwd").value;
 	var pwd2 = document.getElementById("signup-pwd2").value;
 	if(pwd1!=pwd2){
-		alert("Both password fields must be the same");
+		displayUserAlerts("Both password fields must be the same", view, "welcomeview");
 	}else if ((pwd1.length || pwd2.length) < x){
-		alert("The password must contain at least " + x + " characters");
+		displayUserAlerts("The password must contain at least " + x + " characters", view, "welcomeview");
 	}else{
 		var dataObject = {
 			email : document.getElementById("signup-email").value,
@@ -35,7 +35,7 @@ function signupValidation(){
 			country : document.getElementById("country").value
 		};
 		var state = serverstub.signUp(dataObject);
-		alert(state.message);
+		displayUserAlerts(state.message, view, "profileview");
 		if(state.success === true){
 			signIn(dataObject.email, dataObject.password);
 		}
@@ -43,35 +43,33 @@ function signupValidation(){
 }
 
 /*Interprets the form fields fo further use in the sign in process*/
-function loginForm(){
+function loginForm(view){
 	var email = document.getElementById("email").value;
 	var pwd = document.getElementById("pwd").value;
-	signIn(email,pwd);
+	signIn(email,pwd,view);
 }
 
 
-function signIn(email,password){
+function signIn(email,password, view){
 	var state = serverstub.signIn(email, password);
 	if(state.success === true){
 		setToken(state.data);
-		alert(state.message);
 		window.onload();
 	}else{
-		alert(state.message);
+		displayUserAlerts(state.message, view, "welcomeview");
 		var signInForm = document.getElementById('log-in');
 		signInForm.reset();
 	}
 }
 
-function signOut(){
+function signOut(view){
 	var token = getToken();
 	var state = serverstub.signOut(token);
 	if(state.success === true){
-		alert(state.message);
 		localStorage.removeItem('myToken');
 		window.onload();
 	}else{
-		alert(state.message);
+		displayUserAlerts(state.message, view, "profileview");
 	}
 }
 
@@ -79,7 +77,9 @@ function signOut(){
 /*This function uses the input from the tab buttons to interpret
 which tab that is the one that should be shown. The function changes
 the style of the different div tags*/
-function changePanel(active, firstInactive, secondInactive){
+function changePanel(active, firstInactive, secondInactive, view){
+	eraseUserAlerts(view, "profileview");
+
 	active.style.display = 'block';
 	firstInactive.style.display = 'none';
 	secondInactive.style.display = 'none';
@@ -91,22 +91,22 @@ function changePanel(active, firstInactive, secondInactive){
 	}
 }
 
-function changePassword(){
+function changePassword(view){
 	x = 5;
 	var oldPassword = document.getElementById('old-pwd').value;
 	var newPassword = document.getElementById('new-pwd').value;
 	var newPassword2 = document.getElementById('new-pwd2').value;
 	var token = getToken();
 	if(newPassword!=newPassword2){
-		alert("Both new password fields must be the same");
+		displayUserAlerts("Both new password fields must be the same",view, "profileview");
 	}else if ((newPassword.length || newPassword2.length) < x){
-		alert("The  new password must contain at least " + x + " characters");
+				displayUserAlerts("The  new password must contain at least " + x + " characters",view, "profileview");
 	}else{
 		var state = serverstub.changePassword(token, oldPassword, newPassword);
 		if(state.success === true){
-			alert(state.message);
+			displayUserAlerts(state.message, view, "profileview");
 		}else{
-			alert(state.message);
+			displayUserAlerts(state.message, view, "profileview");
 		}
 	}
 	//Erases the form content
@@ -127,18 +127,19 @@ function showMyInformation(){
 
 /*The function interprets what has been written in the search
 field and sets up the user page*/
-function viewUser(){
+function viewUser(view){
 	var token = getToken();
 	var userEmail = document.getElementById('user-email').value;
 	var userData = serverstub.getUserDataByEmail(token, userEmail);
 	var userMessages = serverstub.getUserMessagesByEmail(token, userEmail);
 	if(userData.success === true){
-		alert(userData.message);
+		eraseUserAlerts(view, "profileview");
 		setEmail(userEmail);
 		setUserWallContent();
 		showFriendInfo(userData);
 	}else{
-		alert(userData.message);
+		displayUserAlerts(userData.message, view, "profileview");
+		document.getElementById('user-email').value = "";
 	}	
 }
 
@@ -159,13 +160,13 @@ function showFriendInfo(user){
 /*The function interprets the information of the posting
 textarea and sends it to the function that appends the
 post content on the wall*/
-function getMyPost(){
+function getMyPost(view){
 	var token = getToken();
 	var postContent = document.getElementById('to-my-wall').value;
-	alert(postContent);
 	if(postContent === ""){
-		alert("Write something!");
+		displayUserAlerts("Write something!", view, "profileview");
 	}else{
+		eraseUserAlerts(view, "profileview");
 		var user = serverstub.getUserDataByToken(token);
 		var state = serverstub.postMessage(token, postContent, user.data.email);
 		newestPost(token, user, "#my-wall", "to-my-wall");
@@ -175,16 +176,17 @@ function getMyPost(){
 /*The function interprets the information of the posting
 textarea and sends it to the function that appends the
 post content on the wall*/
-function getUserPost(){
+function getUserPost(view){
 	var token = getToken();
 	var email = getEmail();
 	var postContent = document.getElementById('to-user-wall').value;
 	if(postContent === ""){
-		alert("Write something!");
+		displayUserAlerts("Write something!", view, "profileview");
 	}else if(email === null || email === "undefined"){
-		alert("You need to choose a user!");
+		displayUserAlerts("You need to choose a user!", view, "profileview");
 		document.getElementById('to-user-wall').value = "";
 	}else{
+		eraseUserAlerts(view, "profileview");
 		var email = getEmail();
 		var user = serverstub.getUserDataByEmail(token, email);
 		var state = serverstub.postMessage(token, postContent, user.data.email);
@@ -205,6 +207,7 @@ function newestPost(token, user, messageWall, postWall){
 
 /*The function sets up the content of the logged in users wall*/
 function setWallContent(){
+	$("#message-wall textarea").empty();
 	var token = getToken();
 	var user = serverstub.getUserDataByToken(token);
 	var messages = serverstub.getUserMessagesByEmail(token, user.data.email);
@@ -215,12 +218,12 @@ function setWallContent(){
 }
 
 /*The function sets up the content of the searched users wall*/
-function setUserWallContent(){
+function setUserWallContent(view){
 	$("#user-message-wall textarea").empty();
 	var token = getToken();
 	var email = getEmail();
 	if(email === null || email === "undefined"){
-		alert("You need to choose a user!")
+		displayUserAlerts("You need to choose a user!", view, "profileview");
 	}else{
 		var user = serverstub.getUserDataByEmail(token, email);
 		var messages = serverstub.getUserMessagesByEmail(token, email);
@@ -229,6 +232,28 @@ function setUserWallContent(){
 			$('#user-wall').append(messages.data[i].content + "\n");
 		}
 	}
+}
+
+/*The function appends the current alert message on the appropriate view*/
+function displayUserAlerts(message, view, state){
+	if(state === "welcomeview"){
+		$("#welcomeview-alert-text").empty();
+		$('#welcomeview-alert-text').append(message);
+	}else{
+		$("#profileview-alert-text").empty();
+		$('#profileview-alert-text').append(message);
+	}
+	view.style.backgroundColor = "#B74934";
+}
+
+/*The function erases all non relevant alerts*/
+function eraseUserAlerts(view, state){
+	if(state === "welcomeview"){
+		$("#welcomeview-alert-text").empty();
+	}else{
+		$("#profileview-alert-text").empty();
+	}
+	view.style.backgroundColor = "1a56bb";
 }
 
 /*The functions below are used to make a quick call to
